1e8ca30202f41550f8dbdd05c17c678c7b3a441a
Nmerryman <nmerryman9001@gmail.com>
2025-06-05 04:03:08 -0500

    Updated tests to include project entry

--- test_poc.py
+++ test_poc.py
@@ -1,11 +1,26 @@
 import requests
 
 user = "a"
+user_hash = ord("a")
 
 print("Testing POC")
-print(requests.get("http://127.0.0.1:8000/pull/" + user).json())
+print(requests.get("http://127.0.0.1:8000/pull/" + user, params={"hash": user_hash}).json())
 
-r = requests.post("http://127.0.0.1:8000/push/" + user, json={"data": "test"})
+r = requests.post("http://127.0.0.1:8000/push/" + user, json={"data": "test"}, params={"hash": user_hash})
 print(r.json())
 
-print(requests.get("http://127.0.0.1:8000/pull/" + user).json())
+res = requests.get("http://127.0.0.1:8000/pull/" + user, params={"hash": user_hash}).json()
+print(res)
+if res == {"data": "test"}:
+    print("Passed project test")
+
+user_hash = sum([ord(a) for a in "proj:"]) + user_hash
+print(requests.get("http://127.0.0.1:8000/pull/proj", params={"hash": user_hash, "entry": user}).json())
+
+r = requests.post("http://127.0.0.1:8000/push/proj", json={"data": "test"}, params={"hash": user_hash, "entry": user})
+print(r.json())
+
+res = requests.get("http://127.0.0.1:8000/pull/proj", params={"hash": user_hash, "entry": user}).json()
+print(res)
+if res == {"data": "test"}:
+    print("Passed entry test")
